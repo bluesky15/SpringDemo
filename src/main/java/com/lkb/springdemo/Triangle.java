dab4b071d37a1c148abeccf61b91342eba85bf75
Lalit Behera <lalit143.er@gmail.com>
2019-02-06 19:59:52 +0530

    constructor injection for multiple argument

--- src/main/java/com/lkb/springdemo/Triangle.java
+++ src/main/java/com/lkb/springdemo/Triangle.java
@@ -3,12 +3,28 @@ package com.lkb.springdemo;
 public class Triangle {
     private String type;
 
+
+    private int height;
+
     public Triangle(String type) {
         this.type = type;
     }
 
+    public Triangle(int height) {
+        this.height = height;
+    }
+
+    public Triangle(String type, int height) {
+        this.type = type;
+        this.height = height;
+    }
+
     public void draw() {
-        System.out.println(getType() + " Triangle drawn");
+        System.out.println(getType() + " Triangle drawn with height "+getHeight());
+    }
+
+    public int getHeight() {
+        return height;
     }
 
     public String getType() {
