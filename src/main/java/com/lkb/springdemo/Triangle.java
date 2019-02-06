d6c78f6443e43c6d7e99b8b802b625ba2e38b386
Lalit Behera <lalit143.er@gmail.com>
2019-02-06 19:46:59 +0530

    constructor injection

--- src/main/java/com/lkb/springdemo/Triangle.java
+++ src/main/java/com/lkb/springdemo/Triangle.java
@@ -3,15 +3,15 @@ package com.lkb.springdemo;
 public class Triangle {
     private String type;
 
+    public Triangle(String type) {
+        this.type = type;
+    }
+
     public void draw() {
         System.out.println(getType() + " Triangle drawn");
     }
 
     public String getType() {
         return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
     }
 }
