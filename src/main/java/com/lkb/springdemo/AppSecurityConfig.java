5c52fe88438e2b6529e8175650b4af078c9c1c56
Lalit Behera <lalit143.er@gmail.com>
2019-03-05 08:39:00 +0530

    password retrival form database

--- src/main/java/com/lkb/springdemo/AppSecurityConfig.java
+++ src/main/java/com/lkb/springdemo/AppSecurityConfig.java
@@ -1,12 +1,16 @@
 package com.lkb.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import java.util.ArrayList;
@@ -16,11 +20,21 @@ import java.util.List;
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Bean
-    @Override
-    protected UserDetailsService userDetailsService() {
-        List<UserDetails> users = new ArrayList<>();
-        users.add(User.withDefaultPasswordEncoder().username("Lalit").password("1234").roles("USER").build());
-        return new InMemoryUserDetailsManager(users);
+    public AuthenticationProvider authProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        return provider;
     }
+//    @Bean
+//    @Override
+//    protected UserDetailsService userDetailsService() {
+//        List<UserDetails> users = new ArrayList<>();
+//        users.add(User.withDefaultPasswordEncoder().username("Lalit").password("1234").roles("USER").build());
+//        return new InMemoryUserDetailsManager(users);
+//    }
 }
---
+++ src/main/java/com/lkb/springdemo/UserRepository.java
@@ -0,0 +1,9 @@
+package com.lkb.springdemo;
+
+import com.lkb.springdemo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+}
