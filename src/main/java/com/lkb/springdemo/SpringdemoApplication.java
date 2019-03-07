e9679e47dd322fb9de56b4ecc3c4c20a2a1685b8
Lalit Behera <lalit143.er@gmail.com>
2019-03-07 19:15:25 +0530

    base project added

--- src/main/java/com/lkb/springdemo/SpringdemoApplication.java
+++ src/main/java/com/lkb/springdemo/SpringdemoApplication.java
@@ -1,22 +1,13 @@
 package com.lkb.springdemo;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class SpringdemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringdemoApplication.class, args);
-    }
-}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdemoApplication.class, args);
+	}
 
+}
